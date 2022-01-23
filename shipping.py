7f649c67da2f2482484aa13cd4886ceaeb20a3e2
BlueBlu2 <eng.abdalrhmn.mohamed@gmail.com>
2022-01-23 20:23:40 +0200

    static methods inheritance

--- shipping.py
+++ shipping.py
@@ -25,21 +25,25 @@ class ShippingContainer:
     def __init__(self, owner_code, contents):
         self.owner_code = owner_code
         self.contents = contents
-        self.bic = ShippingContainer._make_bic_code(owner_code=owner_code, serial= ShippingContainer._generate_serial())
+        self.bic = self._make_bic_code(owner_code=owner_code, serial= ShippingContainer._generate_serial())
 
+
+class RefrigeratedShippingContainer(ShippingContainer):
+
+    @staticmethod
+    def _make_bic_code(owner_code, serial):
+        return iso6346.create(owner_code=owner_code,serial=str(serial).zfill(6),category="R")
 
 c4 = ShippingContainer('hai', ["go"])
 c5 = ShippingContainer('hia', ["go"])
-c6 = ShippingContainer('hiw', ["go"])
-c7 = ShippingContainer('hie', ["go"])
-c9 = ShippingContainer('hic', ["go"])
+r4 = RefrigeratedShippingContainer('hiw', ["go"])
+r5 = RefrigeratedShippingContainer('hie', ["go"])
 print(c4.bic)
 print(c4.next_serial)
 print(c5.bic)
 print(c5.next_serial)
-print(c6.bic)
-print(c6.next_serial)
-print(c7.bic)
-print(c7.next_serial)
-print(c9.bic)
-print(c9.next_serial)
+print(r4.bic)
+print(r4.next_serial)
+print(r5.bic)
+print(r5.next_serial)
+
